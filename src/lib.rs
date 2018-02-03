7e729d29acbe515a19725eea8b83dcbe7d5c3ea4
Declan Kelly <dkelly.home@gmail.com>
2018-02-03 03:15:07 -0800

    Bump version, add different nightly features.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-#![feature(allocator_api, pointer_methods)]
+#![feature(allocator_api, pointer_methods, attr_literals, shared)]
 #![warn(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/log/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/log/0.1.2")]
 
 //! A counting network is a type of concurrent data structure that gives non-blocking access to specific
 //! operations, most commonly ``fetch-and-inc``.
